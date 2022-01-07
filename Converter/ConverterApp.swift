f12a747c5c8a4dc9da542d08357292fa4b5dab07
makhmudov0907 <71522647+makhmudov0907@users.noreply.github.com>
2022-01-07 17:33:19 +0500

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Mirshod Makhmudov on 07/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
